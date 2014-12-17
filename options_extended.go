7b4c6d288c37e04b16ccc996d773860cc6286e1f
Nikita Vershinin <endeveit@gmail.com>
2014-12-17 20:17:08 +0600

    Replaced "ptidy" with "tidyp" in comments

--- options_extended.go
+++ options_extended.go
@@ -8,19 +8,19 @@ import (
 
 // This option controls the deletion or addition of the name attribute in elements where it can serve as anchor. If set to "true", a name attribute, if not already existing, is added along an existing id attribute if the DTD allows it. If set to "false", any existing name attribute is removed if an id attribute exists or has been added.
 func (this *Tidy) AnchorAsName(val bool) (bool, error) {
-	// C.TidyAnchorAsName = 87 it doesn't exists in ptidy
+	// C.TidyAnchorAsName = 87 it doesn't exists in tidyp
 	return this.optSetBool(87, cBool(val))
 }
 
 // Can be used to modify behavior of -c (--clean yes) option. This option specifies if Tidy should merge nested <span> such as "<span><span>...</span></span>". The algorithm is identical to the one used by --merge-divs.
 func (this *Tidy) MergeSpans(val int) (bool, error) {
-	// C.TidyMergeSpans = 86 it doesn't exists in ptidy
+	// C.TidyMergeSpans = 86 it doesn't exists in tidyp
 	return this.optSetAutoBool(86, (C.ulong)(val))
 }
 
 // This option specifies if Tidy should preserve the well-formed entitites as found in the input.
 func (this *Tidy) PreserveEntities(val bool) (bool, error) {
-	// C.TidyPreserveEntities = 85 it doesn't exists in ptidy
+	// C.TidyPreserveEntities = 85 it doesn't exists in tidyp
 	return this.optSetBool(85, cBool(val))
 }
 
@@ -28,7 +28,7 @@ func (this *Tidy) PreserveEntities(val bool) (bool, error) {
 func (this *Tidy) SortAttributes(val int) (bool, error) {
 	switch val {
 	case None, Alpha:
-		// C.TidySortAttributes = 84 it doesn't exists in ptidy
+		// C.TidySortAttributes = 84 it doesn't exists in tidyp
 		return this.optSetInt(84, (C.ulong)(val))
 	}
 	return false, errors.New("Argument val int is out of range (0,1)")
